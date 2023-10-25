f4e77d5c50f23d4ed7db8f21254af3df59aac178
arunpshankar <arunprshankar@gmail.com>
2023-10-25 14:50:12 -0400

    wip

--- src/prune/llm.py
+++ src/prune/llm.py
@@ -3,51 +3,96 @@ from langchain.prompts.chat import HumanMessagePromptTemplate
 from langchain.prompts.chat import ChatPromptTemplate
 from langchain.chat_models import ChatVertexAI
 from src.config.logging import logger
+from src.config.setup import config
+from typing import List, Dict
+import jsonlines
 import json
-
 
 MODEL_NAME = 'chat-bison'
 
 class LLM:
-    def __init__(self):
-        # Initialize topics list
-        self.topics = []
-        
-        # Load the model
-        self.model = self.load_model()
-        
-        # Load topics from the provided JSONL file
-        self.load_jsonl('./config/topics.jsonl')
+    """Language Learning Model for classifying finance-related PDFs based on topics."""
+    
+    def __init__(self, topics_filepath: str = './config/topics.jsonl'):
+        """Initialize the LLM instance by loading topics and model."""
+        self.topics = self._load_topics_from_jsonl(topics_filepath)
+        self.model = self._initialize_model()
 
-    def load_model(self):
+    def _initialize_model(self) -> ChatVertexAI:
         """Load the chat model from Vertex AI."""
-        model = ChatVertexAI(model_name=MODEL_NAME, temperature=0, max_output_tokens=512, verbose=True)
-        return model
+        try:
+            return ChatVertexAI(model_name=MODEL_NAME, temperature=0, max_output_tokens=512, verbose=True)
+        except Exception as e:
+            logger.error(f"Failed to load the model: {e}")
+            raise
 
-    def load_jsonl(self, filepath):
-        """Load the topics from the provided JSONL file."""
-        with open(filepath, 'r') as file:
-            for line in file:
-                self.topics.append(json.loads(line))
-
-    def construct_prompt(self, page_content, pdf_url):
-        pass
-
-    def classify(self, prompt):
+    def _load_topics_from_jsonl(self, filepath: str) -> List[str]:
+        """Load topics from the provided JSONL file."""
+        topics = []
+        try:
+            with jsonlines.open(filepath) as reader:
+                for topic in reader:
+                    topics.append(topic)
+        except Exception as e:
+            logger.error(f"Failed to load topics from {filepath}: {e}")
+            raise
+        return topics
+    
+    def _get_topics_text(self) -> str:
         """
-        Get the model's classification based on the prompt.
-        
-        Parameters:
-        - prompt: Text prompt for classification
+        Convert the topics into a formatted text representation.
         
         Returns:
-        - completion: Model's response
+            str: Formatted text representation of the topics.
         """
-        response = self.model(prompt)
-        completion = response.content.strip()
-        return completion
+        topics_details = []
+        for topic in self.topics:
+            topic_info = (f"Topic: {topic['type']}\n"
+                        f"Definition: {topic['definition']}\n"
+                        f"Synonyms: {topic['synonyms']}\n")
+            topics_details.append(topic_info)
+            
+        return '\n'.join(topics_details)
 
 
-if __name__ == '__main__':
-    llm = LLM()
-    llm.classify('')
+    @staticmethod
+    def _convert_to_text_template(data: Dict[str, any]) -> str:
+        """Convert a dictionary into a text template format with key in uppercase followed by value."""
+        return '\n'.join([f"{key.upper()}: {value}" for key, value in data.items() if value is not None])
+
+    def _construct_prompt(self, metadata: dict) -> str:
+        """Construct a prompt based on the metadata."""
+        pdf_url = metadata.pop('link', None)
+        metadata_text = self._convert_to_text_template(metadata)
+        topics_text = self._get_topics_text()
+        
+        system_template = f"""You are a finance research analyst. Your task is to classify the provided PDF URL and its metadata based on the list of topics below. 
+If the PDF url or its metadata contains any terms related to the topics, classify accordingly. If you cannot classify, label it as 'unclassified'.
+
+== TOPICS ==
+{topics_text}"""
+        system_message_prompt = SystemMessagePromptTemplate.from_template(template=system_template)
+        human_template = f"""== PDF URL ==
+{pdf_url}
+
+== METADATA ==
+{metadata_text}
+
+Provide your classification along with the rationale behind your decision. 
+Highlight any specific elements from the PDF URL or metadata that influenced your choice. 
+If unclassified, explain why. 
+Your response should be in a structured JSON format with two fields: `classification` and `rationale`.
+NOTE: Focus on the SNIPPET under METADATA."""
+        human_message_prompt = HumanMessagePromptTemplate.from_template(template=human_template)  
+        chat_prompt = ChatPromptTemplate(messages=[system_message_prompt, human_message_prompt])
+        prompt = chat_prompt.format_messages()
+        return prompt
+
+    def classify(self, metadata: dict) -> str:
+        """Get the model's classification based on the provided metadata."""
+        prompt = self._construct_prompt(metadata)
+        try:
+            response = self.model(prompt)
+            return response.content.strip()
+        except Exception as e:
+            logger.error(f"Failed to classify: {e}")
--- src/prune/pruner.py
+++ src/prune/pruner.py
@@ -1,3 +1,4 @@
+from src.config.logging import logger
 from src.prune.llm import LLM
 import json
 import json
@@ -5,10 +6,7 @@ import json
 
 class Pruner:
     def __init__(self):
-        # Define base directories for easier reference later
-        self.base_pdf_dir = './data/collected_pdfs/'
-        self.base_url_dir = './data/crawled_urls/'
-        self.base_metadata_dir = './data/selected_urls/'
+        self.llm = LLM()
 
     def is_pdf_url(self, url):
         """
@@ -37,74 +35,10 @@ class Pruner:
         except json.JSONDecodeError:
             print(f"Error decoding JSON for URL: {child_url}. Content: {clean_response_str}")
             return 'Unclassified', 'Error decoding response'
+        
 
+    def prune(self):
+        pass
 
-    def download_pdf(self, url, company_name, file_name):
-        """
-        Download the PDF from the given URL and save it with the specified filename.
-        """
-        response = requests.get(url)
-        company_dir = os.path.join(self.base_pdf_dir, company_name)
-        if not os.path.exists(company_dir):
-            os.makedirs(company_dir)
-        with open(os.path.join(company_dir, f'{file_name}.pdf'), 'wb') as file:
-            file.write(response.content)
-
-    def append_metadata(self, metadata, company_name):
-        """
-        Append the classification and rationale as metadata to the company's JSONL file.
-        """
-        with open(os.path.join(self.base_metadata_dir, f'{company_name}_metadata.jsonl'), 'a') as file:
-            json.dump(metadata, file)
-            file.write('\n')  # New line for next JSON entry
-
-    def process_single_line(self, line, company_name):
-        llm_instance = LLM()
-
-        data = json.loads(line)
-        child_url = data['child']
-        if self.is_pdf_url(child_url):
-            pdf_name = child_url.split('/')[-1].replace('.pdf', '')
-            print(f'Processing URL: {child_url}')
-
-            response = requests.get(data['parent'])
-            soup = BeautifulSoup(response.text, 'html.parser')
-
-            classification, rationale = self.classify_content(soup.text, child_url, llm_instance)
-            if classification.lower() != 'unclassified':
-                self.download_pdf(child_url, company_name, pdf_name)
-
-                metadata = {
-                    'company': company_name,
-                    'filename': pdf_name,
-                    'classification': classification,
-                    'rationale': rationale
-                }
-                self.append_metadata(metadata, company_name)
-
-    def process_files(self):
-        """
-        Process all the JSONL files in the data/crawled_urls/ directory.
-        """
-        # Ensure required directories exist
-        if not os.path.exists(self.base_pdf_dir):
-            os.makedirs(self.base_pdf_dir)
-        if not os.path.exists(self.base_metadata_dir):
-            os.makedirs(self.base_metadata_dir)
-
-        # Use a Pool of workers
-        with Pool(cpu_count()) as pool:
-            # Iterate over all the JSONL files in the directory
-            for filename in os.listdir(self.base_url_dir):
-                with open(os.path.join(self.base_url_dir, filename), 'r') as file:
-                    company_name = filename.split('.')[0]
-                    try:
-                        # Use starmap to pass multiple arguments to process_single_line
-                        pool.starmap(self.process_single_line, [(line, company_name) for line in file])
-                    except Exception as e:
-                        print(f"Error processing file {filename}. Reason: {e}")
-                        continue  
-               
 if __name__ == '__main__':
-    pruner = Pruner()
-    pruner.process_files()
+    pass
