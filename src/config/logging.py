7fe6b81fe917de68b28d6deb5bed8929a75dedb6
arunpshankar <arunprshankar@gmail.com>
2023-10-25 07:19:44 -0400

    wip

--- src/config/logging.py
+++ src/config/logging.py
@@ -12,7 +12,7 @@ def setup_logger(log_filename="app.log", log_dir="logs"):
     # Define the logging configuration
     logging.basicConfig(
         level=logging.INFO,  # Log level: DEBUG, INFO, WARNING, ERROR, CRITICAL
-        format="%(asctime)s [%(levelname)s]: %(message)s",  # Format of the log message
+        format="%(asctime)s [%(levelname)s] [%(module)s] [%(pathname)s]: %(message)s",  # Include module and directory in the log message
         handlers=[
             logging.StreamHandler(),  # Log to console
             logging.FileHandler(log_filepath)  # Log to file
