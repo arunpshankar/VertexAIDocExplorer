c80a68c8cb6fbfb45135913246b86539b6cd354e
arunpshankar <arunprshankar@gmail.com>
2023-10-30 09:00:31 -0400

    wip

--- src/search/doc_search.py
+++ src/search/doc_search.py
@@ -93,7 +93,7 @@ def transform_search_results(question: str, search_results: List[Dict[str, Any]]
         transformed_results.append(transformed_result)
     return transformed_results
 
-def save_to_jsonl(data: List[Dict[str, Any]], filename: str = "results.jsonl") -> None:
+def save_to_jsonl(data: List[Dict[str, Any]], filename: str = "./data/doc-search-results.jsonl") -> None:
     """
     Save the provided data to a .jsonl file.
 
@@ -104,33 +104,3 @@ def save_to_jsonl(data: List[Dict[str, Any]], filename: str = "results.jsonl") -
     with jsonlines.open(filename, mode='w') as writer:
         for item in data:
             writer.write(item)
-
-def get_and_save_results(query: str) -> None:
-    """
-    Process a user query to get search results, transform them into the updated JSON structure, display them, 
-    and save them to a .jsonl file.
-
-    Args:
-        query (str): The user's query.
-    """
-    conversation_id = create_conversation()
-    response = chat(query, conversation_id)
-
-    summary_text = response['reply']['summary']['summaryText']
-    
-    search_results = response['searchResults']
-    transformed_results = transform_search_results(query, search_results, summary_text)
-    
-    # Displaying the results
-    for result in transformed_results:
-        print(f"Rank = {result['rank']} | Document = {result['document']} | Answer = {result['answer']}")
-        for segment in result['segments']:
-            print(f"Segment = {segment['segment']} | Page = {segment['page']}")
-        print('=' * 120)
-
-    # Saving the results to a .jsonl file
-    save_to_jsonl(transformed_results)
-
-
-if __name__ == '__main__':
-    get_and_save_results(query='what is hsbc LCR 2021')
