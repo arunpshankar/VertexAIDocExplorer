23a9b344c06edccc133d26ff125c88fa3c9e8386
arunpshankar <arunprshankar@gmail.com>
2023-10-30 16:56:16 -0400

    wip

---
+++ src/evaluate/doc_search.py
@@ -0,0 +1,55 @@
+from src.search.doc_search import transform_search_results
+from src.search.doc_search import create_conversation
+from src.search.doc_search import chat
+import jsonlines 
+import time
+
+
+def evaluate_doc_search(inp_file_path: str, out_file_path: str) -> None:
+    """
+    Evaluates the document search based on the given questions in the input file.
+    Writes the results to the output file.
+
+    Parameters:
+    - inp_file_path (str): Path to the input file containing questions for document search.
+    - out_file_path (str): Path to the output file where search results will be written.
+
+    Returns:
+    - None
+    """
+
+    with jsonlines.open(inp_file_path, 'r') as reader, jsonlines.open(out_file_path, 'w') as writer:
+        for data in reader:
+            question = data.get('question')
+            if not question:
+                continue
+
+            print(f"Question: {question}")
+
+            conversation_id = create_conversation()
+            response = chat(question, conversation_id)
+
+            summary_text = response['reply']['summary']['summaryText']
+            search_results = response['searchResults']
+            transformed_results = transform_search_results(question, search_results, summary_text)
+            
+            # Displaying the results and writing to the output file
+            for result in transformed_results:
+                print(f"Rank = {result['rank']} | Document = {result['document']} | Answer = {result['answer']}")
+                segments_data = []
+                for segment in result['segments']:
+                    print(f"Segment = {segment['segment']} | Page = {segment['page']}")
+                    segments_data.append({"segment": segment['segment'], "page": segment['page']})
+                writer.write({
+                    "question": question,
+                    "rank": result['rank'],
+                    "document": result['document'],
+                    "segments": segments_data,
+                    "answer": result['answer']
+                })
+                print('=' * 120)
+            time.sleep(5)  # to avoid API throttling
+
+
+if __name__ == '__main__':
+    evaluate_doc_search('./config/doc-search-queries.jsonl', './data/evaluate/doc-search-results.jsonl')
--- src/evaluate/site_search.py
+++ src/evaluate/site_search.py
@@ -10,26 +10,38 @@ def evaluate_site_search(inp_file_path: str, out_file_path: str) -> None:
     Evaluate the site search for each query in the input file and save the results in the output file.
 
     Args:
-        inp_file_path (str): Path to the input JSONL file containing the queries.
-        out_file_path (str): Path to the output JSONL file to save the results.
-
-    Returns:
-        None
+        inp_file_path (str): Path to the input JSONL file containing the evaluation queries.
+        out_file_path (str): Path to the output JSONL file to save the evaluation site search results.
     """
     with jsonlines.open(inp_file_path, 'r') as reader, jsonlines.open(out_file_path, 'w') as writer:
         for data in reader:
             query = data.get('query')
-            if query:
-                results = fetch_all_results(query)
-                for i, result in enumerate(results):
-                    rank = i+1
-                    discovery_response = DiscoveryResponse(query, result, rank)
-                    logger.info(discovery_response.to_dict())
-                    writer.write(discovery_response.to_dict())
-                    logger.info('-' * 100)
+            
+            if not query:
+                logger.warning(f"Missing query in data: {data}")
+                continue
+
+            results = fetch_all_results(query)
+
+            for i, result in enumerate(results):
+                rank = i + 1
+                discovery_response = DiscoveryResponse(query, result, rank)
+                
+                logger.info(discovery_response.to_dict())
+                
+                writer.write(discovery_response.to_dict())
+                
+            logger.info('-' * 100)
 
 
 def evaluate_pruner(inp_file_path: str, out_file_path: str) -> None:
+    """
+    Evaluate and prune results based on certain criteria.
+
+    Args:
+        inp_file_path (str): Path to the input JSONL file containing the site search results.
+        out_file_path (str): Path to the output JSONL file to save the pruned results.
+    """
     pruner = Pruner()
     pruner.prune(inp_file_path, out_file_path)
 
