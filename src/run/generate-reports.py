d952b1f7177a91d89e1c579c041afcddaf674ee2
arunpshankar <arunprshankar@gmail.com>
2023-10-31 13:36:58 -0400

    wip

--- src/run/generate-reports.py
+++ src/run/generate-reports.py
@@ -1,6 +1,8 @@
-from src.utils.reports import jsonl_to_excel
+from src.utils.reports import jsonl_to_excel_site_search
+from src.utils.reports import jsonl_to_excel_doc_search
 
 
 if __name__ == '__main__':
-    jsonl_to_excel('./data/evaluate/site-search-results.jsonl',  './data/evaluate/site-search-results.xlsx')
-    jsonl_to_excel('./data/evaluate/site-search-results-pruned.jsonl',  './data/evaluate/site-search-results-pruned.xlsx')
+    jsonl_to_excel_site_search("./data/evaluate/site-search-results.jsonl", "./data/evaluate/site-search-results.xlsx")
+    jsonl_to_excel_site_search("./data/evaluate/site-search-results-pruned.jsonl", "./data/evaluate/site-search-results-pruned.xlsx")
+    jsonl_to_excel_doc_search("./data/evaluate/doc-search-results.jsonl", "./data/evaluate/doc-search-results.xlsx")
--- src/utils/reports.py
+++ src/utils/reports.py
@@ -157,9 +157,3 @@ def jsonl_to_excel_doc_search(input_path: str, output_path: str):
 
     wb.save(output_path)
     logger.info(f"Successfully saved formatted data to {output_path}.")
-
-
-if __name__ == '__main__':
-    jsonl_to_excel_site_search("./data/evaluate/site-search-results.jsonl", "./data/evaluate/site-search-results.xlsx")
-    jsonl_to_excel_site_search("./data/evaluate/site-search-results-pruned.jsonl", "./data/evaluate/site-search-results-pruned.xlsx")
-    jsonl_to_excel_doc_search("./data/evaluate/doc-search-results.jsonl", "./data/evaluate/doc-search-results.xlsx")
